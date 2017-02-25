7b9e27422ef41922a0a100e729246fbdcadab010
Angela Upreti <angelaupreti@Angelas-MacBook-Pro.local>
2017-02-25 16:01:35 -0500

    locations

--- lab1.py
+++ lab1.py
@@ -12,16 +12,17 @@ def matrix_creation(size):
     matrix = np.zeros((size, size), dtype = int)
     directions = [[-1,0],[1,0],[0,-1],[0,1],[1,1],[-1,1],[1,-1],[-1,-1]]
     location_list = []
-    
-    for i in range(6):
-        pig_location = np.random.choice(size,2, replace = False)
+
+    for i in range(num_pigs):
+        pig_location = np.random.choice(size,2, replace = True)
+        while [pig_location[0],pig_location[1]] in location_list:
+            pig_location = np.random.choice(size,2, replace = True)
         matrix[pig_location[0],pig_location[1]] = 1
-        location_list.append(pig_location)
-    
+        location_list.append([pig_location[0],pig_location[1]])
     pdb.set_trace()
+
     return location_list
 
 
 if __name__ == '__main__':
       matrix_creation(4)
-     
