28834af6000baf60fc105df2a94938f721b6a3a4
santiagocorrea <santiago.correa.cardona@gmail.com>
2017-02-25 16:05:29 -0500

    old version

--- lab1.py
+++ lab1.py
@@ -1,5 +1,6 @@
 import pdb
 import numpy as np
+import random
 '''
 Matrix Implementation
 
@@ -12,9 +13,11 @@ def matrix_creation(size):
     matrix = np.zeros((size, size), dtype = int)
     directions = [[-1,0],[1,0],[0,-1],[0,1],[1,1],[-1,1],[1,-1],[-1,-1]]
     location_list = []
+    np.asarray(location_list)
     
-    for i in range(6):
+    while location_list < 6:
         pig_location = np.random.choice(size,2, replace = False)
+        if pig_location is in location_list:
         matrix[pig_location[0],pig_location[1]] = 1
         location_list.append(pig_location)
     
